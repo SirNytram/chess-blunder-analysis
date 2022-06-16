300602c607e702c1c3cf7e2039d8e30ede87c8f1
Martin Cote <martcote@gmail.com>
2022-06-16 03:05:30 -0400

    add graph, add detailed option

--- main.py
+++ main.py
@@ -14,122 +14,137 @@ def my_form():
 @app.route('/', methods=['POST'])
 def my_form_post():
     pgn = request.form['pgn']
+    moves = []
+
+    detailed = request.form.get('detailed')
 
     output = ''
 
 
-    # clipboard = pyperclip.paste()
-    f = open('game.pgn', 'w')
-    f.write(pgn)
-    f.close()
+    if pgn != '':
+        # clipboard = pyperclip.paste()
+        f = open('game.pgn', 'w')
+        f.write(pgn)
+        f.close()
 
-    #.\pgn-extract.exe --quiet -C --fencomments -w 100000 --output game-fen.pgn game.pgn
-    os.system('.\\pgn-extract.exe --quiet -C --fencomments -w 100000 --output game-fen.pgn game.pgn')
-    stockfish = Stockfish('stockfish_15_x64_avx2.exe') #path="/Users/zhelyabuzhsky/Work/stockfish/stockfish-9-64")
-    # stockfish.set_fen_position("rnbqkb1r/ppp2ppp/3p4/4P3/4n3/5N2/PPP2PPP/RNBQKB1R w KQkq - 0 5")
-    # print(stockfish.get_top_moves(3))
-    
-    
-
-
-    fn = 'game-fen.pgn'
-
-    with open(fn, encoding='utf-8') as h:
-        game = chess.pgn.read_game(h)
-
-        moves = []
-        topstr = ''
-        prev_score = None
-        prev_fen = None
-        moveno = 1.0
-
+        #.\pgn-extract.exe --quiet -C --fencomments -w 100000 --output game-fen.pgn game.pgn
+        os.system('.\\pgn-extract.exe --quiet -C --fencomments -w 100000 --output game-fen.pgn game.pgn')
+        stockfish = Stockfish('stockfish_15_x64_avx2.exe') #path="/Users/zhelyabuzhsky/Work/stockfish/stockfish-9-64")
+        # stockfish.set_fen_position("rnbqkb1r/ppp2ppp/3p4/4P3/4n3/5N2/PPP2PPP/RNBQKB1R w KQkq - 0 5")
+        # print(stockfish.get_top_moves(3))
+        # print(stockfish.get_parameters())
+        # stockfish.set_elo_rating(500)
         
-        for node in game.mainline():
-            fen = node.comment
-            # com = comment.split('\n')
-            # fen = ''
-            # val = com[0]
-            # if len(com) > 1:
-            #     fen = com[1]
-
-            stockfish.set_fen_position(fen)
-            score = stockfish.get_evaluation()
-            square = f'{node.move}'[2:4]
-            piece = f'{node.board().piece_at(chess.parse_square(square))}'.upper()
-            top_moves = [] #stockfish.get_top_moves(5)
-
-            movenostr = f'{moveno: >2.0f}'
-            is_white = True
-            if moveno % 1 != 0:
-                movenostr = '  '
-                is_white = False
-
-            score_diff = 0
-            if prev_score:
-                score_diff = score['value'] - prev_score['value']
-
-            
-            highlight_suf = f'({score_diff/100:+.1f})'.replace('+', ' ')
-            if (is_white and score_diff < -450) or (not is_white and score_diff > 450):
-                    highlight= f'BLUNDER'
-            elif abs(score_diff) >= 150:
-                    highlight= f'  ***  '
-            elif abs(score_diff) >= 100:
-                    highlight= f'   *   '
-            else:
-                    highlight= f'       ' 
-                    #highlight_suf = ''
 
 
+        fn = 'game-fen.pgn'
 
-            if score['type'] == 'cp':
-                scorestr = f'{score["value"]/100:+.1f}'.replace('+', ' ')
-            else:
-                scorestr = f'M{score["value"]}'
-
-            if prev_fen:
-                if abs(score_diff) >= 450:
-                    stockfish.set_fen_position(prev_fen)
-                    top_moves = stockfish.get_top_moves(3)
+        with open(fn, encoding='utf-8') as h:
+            game = chess.pgn.read_game(h)
 
             topstr = ''
-            top_moves_str = []
-            for cur_top_move in top_moves:
-                top_square1_src = f'{cur_top_move["Move"]}'[0:2]
-                top_piece1 = f'{prev_board.piece_at(chess.parse_square(top_square1_src))}'.upper()
-                top_square1 = f'{cur_top_move["Move"]}'[2:4]
-                top_centipawn1 = cur_top_move['Centipawn']
-                if top_centipawn1:
-                    top_centipawn1 = f'{top_centipawn1/100:+.1f}'.replace('+', ' ')
-                if not top_centipawn1:
-                    top_centipawn1 = ''
+            prev_score = None
+            prev_fen = None
+            moveno = 1.0
 
-                top_mate = ''
-                if cur_top_move['Mate']:
-                    top_mate = f'M{cur_top_move["Mate"]}'
+            
+            for node in game.mainline():
+                fen = node.comment
+                # com = comment.split('\n')
+                # fen = ''
+                # val = com[0]
+                # if len(com) > 1:
+                #     fen = com[1]
 
-                topstr = f'{topstr} {top_piece1}{top_square1} ({top_centipawn1}{top_mate})    '
-                top_moves_str.append([top_piece1, top_square1, top_centipawn1, top_mate])
+                stockfish.set_fen_position(fen)
+                score = stockfish.get_evaluation()
+                square = f'{node.move}'[2:4]
+                piece = f'{node.board().piece_at(chess.parse_square(square))}'.upper()
+                top_moves = [] #stockfish.get_top_moves(5)
 
-            if len(top_moves_str) == 0:
-                top_moves_str = ['','','']
+                movenostr = f'{moveno: >2.0f}'
+                is_white = True
+                if moveno % 1 != 0:
+                    movenostr = '  '
+                    is_white = False
 
-            notfound = ' '
-            if topstr.find(f'{piece}{square}') == -1 and topstr != '':
-                notfound = '*'
+                score_diff = 0
+                if prev_score:
+                    score_diff = score['value'] - prev_score['value']
 
-            line = f'{highlight} {movenostr}  ({scorestr})  {piece}{square}{notfound}            {topstr} {highlight_suf}'
-            output += f'{line}<br>\n'
-            moves.append([highlight, movenostr, scorestr, piece, square, top_moves_str, highlight_suf])
-            print(line)
+                
+                highlight_suf = f'{score_diff/100:+.1f}'.replace('+', ' ')
+                if (is_white and score_diff < -450) or (not is_white and score_diff > 450):
+                        highlight= f'BLUNDER'
+                elif abs(score_diff) >= 150:
+                        highlight= f'  ***  '
+                elif abs(score_diff) >= 100:
+                        highlight= f'   *   '
+                else:
+                        highlight= f'       ' 
+                        #highlight_suf = ''
 
-            moveno+=0.5
-            node_prev = node
-            prev_score = score
-            prev_fen = fen
-            prev_board = node.board()
-            # print(stockfish.get_top_moves(3))
-            # print(stockfish.get_best_move())
+
+
+                if score['type'] == 'cp':
+                    scorestr = f'{score["value"]/100:+.1f}'.replace('+', ' ')
+                    scoregraph = f'{score["value"]/100:.1f}'
+                else:
+                    scorestr = f'M{score["value"]}'
+                    if score["value"] == 0:
+                        if is_white:
+                            scoregraph = 30
+                        else:
+                            scoregraph = -30
+
+                    elif score["value"] > 0:
+                        scoregraph = f'{30 - (score["value"])}'
+                    else:
+                        scoregraph = f'{-30 + (score["value"] * -1)}'
+
+                if prev_fen and detailed:
+                    if abs(score_diff) >= 450:
+                        stockfish.set_fen_position(prev_fen)
+                        top_moves = stockfish.get_top_moves(3)
+
+                topstr = ''
+                top_moves_str = []
+                for cur_top_move in top_moves:
+                    top_square1_src = f'{cur_top_move["Move"]}'[0:2]
+                    top_piece1 = f'{prev_board.piece_at(chess.parse_square(top_square1_src))}'.upper()
+                    top_square1 = f'{cur_top_move["Move"]}'[2:4]
+                    top_centipawn1 = cur_top_move['Centipawn']
+                    if top_centipawn1:
+                        top_centipawn1 = f'{top_centipawn1/100:+.1f}'.replace('+', ' ')
+                    if not top_centipawn1:
+                        top_centipawn1 = ''
+
+                    top_mate = ''
+                    if cur_top_move['Mate']:
+                        top_mate = f'M{cur_top_move["Mate"]}'
+
+                    topstr = f'{topstr} {top_piece1}{top_square1} ({top_centipawn1}{top_mate})    '
+                    top_moves_str.append([top_piece1, top_square1, top_centipawn1, top_mate])
+
+                if len(top_moves_str) == 0:
+                    top_moves_str = ['','','']
+
+                notfound = ' '
+                if topstr.find(f'{piece}{square}') == -1 and topstr != '':
+                    notfound = '*'
+
+                line = f'{highlight} {movenostr}  ({scorestr})  {piece}{square}{notfound}            {topstr} {highlight_suf}'
+                output += f'{line}<br>\n'
+                moves.append([highlight, movenostr, scorestr, piece, square, top_moves_str, highlight_suf, scoregraph])
+                print(line)
+
+                moveno+=0.5
+                node_prev = node
+                prev_score = score
+                prev_fen = fen
+                prev_board = node.board()
+                # print(stockfish.get_top_moves(3))
+                # print(stockfish.get_best_move())
     # return output
     return render_template('index.html', moves=moves,pgn=pgn)
 
