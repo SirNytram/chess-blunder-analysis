9d20435bd8cc54a35b1ee591d9f762ad50baaf4a
Martin Cote <martcote@gmail.com>
2022-06-17 11:39:15 -0400

    speedup by having time limit

--- main.py
+++ main.py
@@ -4,6 +4,58 @@ import os
 from stockfish import Stockfish
 import chess
 import chess.pgn
+import chess.engine
+import time
+engine = chess.engine.SimpleEngine.popen_uci("stockfish_15_x64_avx2.exe")
+
+
+
+VALUE_MATE = 3200
+
+
+def mate_to_value(mate: int) -> int:
+    """
+    Convert mate number to value.
+    """
+    if mate > 0:
+        v = VALUE_MATE - 2*mate + 1
+    else:
+        v = -VALUE_MATE - 2*mate
+
+    return v
+
+
+def my_get_evaluation(fish: Stockfish, fen: str, timems: int):
+    """
+    Evaluate the fen with fish at a given timems.
+    Returns a dict of score {'cp': '49', 'mate': None} and move.
+    """
+    score = {}
+    bestmove = '0000'
+
+    fish.set_fen_position(fen)
+    res = fish.get_best_move_time(timems)
+
+    search_info = fish.info
+    try:
+        bestmove = search_info.split(' pv ')[1].split()[0]
+
+        if 'score cp ' in search_info:
+            score_cp = search_info.split('score cp ')[1].split()[0]
+            score.update({'cp': int(score_cp), 'mate': None})
+        elif 'score mate ' in search_info:
+            score_mate = search_info.split('score mate ')[1].split()[0]
+            score_cp = mate_to_value(int(score_mate))
+            score.update({'cp': int(score_cp), 'mate': int(score_mate)})
+    except:
+        print('error splitting')
+        score.update({'cp': int(0), 'mate': None})
+
+    return score, bestmove
+
+def current_milli_time():
+    return round(time.time() * 1000)
+
 
 app = Flask(__name__)
 
@@ -18,8 +70,10 @@ def my_form_post():
 
     detailed = request.form.get('detailed')
 
-    output = ''
+    think_time = request.form.get('think_time')
 
+    output = ''
+    start_time = current_milli_time()
 
     if pgn != '':
         # clipboard = pyperclip.paste()
@@ -56,8 +110,18 @@ def my_form_post():
                 # if len(com) > 1:
                 #     fen = com[1]
 
-                stockfish.set_fen_position(fen)
-                score = stockfish.get_evaluation()
+                # score, bm = my_get_evaluation(stockfish, fen, 10)
+
+                info = engine.analyse(node.board(), chess.engine.Limit(time=float(think_time)))
+                score = info['score']
+                
+
+
+                # stockfish.set_fen_position(fen)
+                # stockfish.get_best_move_time(100)
+                # score = stockfish.get_evaluation()
+                # print(stockfish.info)
+
                 square = f'{node.move}'[2:4]
                 piece = f'{node.board().piece_at(chess.parse_square(square))}'.upper()
                 top_moves = [] #stockfish.get_top_moves(5)
@@ -69,8 +133,8 @@ def my_form_post():
                     is_white = False
 
                 score_diff = 0
-                if prev_score:
-                    score_diff = score['value'] - prev_score['value']
+                if not score.is_mate() and prev_score and not prev_score.is_mate():
+                    score_diff = score.pov(chess.WHITE).score() - prev_score.pov(chess.WHITE).score()
 
                 
                 highlight_suf = f'{score_diff/100:+.1f}'.replace('+', ' ')
@@ -85,22 +149,31 @@ def my_form_post():
                         #highlight_suf = ''
 
 
-
-                if score['type'] == 'cp':
-                    scorestr = f'{score["value"]/100:+.1f}'.replace('+', ' ')
-                    scoregraph = f'{score["value"]/100:.1f}'
+                if score.is_mate():
+                    scorestr = f'M{score.pov(chess.WHITE).mate()}'
+                    scoregraph = score.pov(chess.WHITE).score(mate_score = 30)
+                    # if is_white:
+                    #     scoregraph = 30 - score.relative.moves
+                    # else:
+                    #     scoregraph = -30 - score.relative.moves
                 else:
-                    scorestr = f'M{score["value"]}'
-                    if score["value"] == 0:
-                        if is_white:
-                            scoregraph = 30
-                        else:
-                            scoregraph = -30
+                    scorestr = f'{score.pov(chess.WHITE).score()/100:+.1f}'.replace('+', ' ')
+                    scoregraph = scorestr
+                # if score['type'] == 'cp':
+                #     scorestr = f'{score["value"]/100:+.1f}'.replace('+', ' ')
+                #     scoregraph = f'{score["value"]/100:.1f}'
+                # else:
+                #     scorestr = f'M{score["value"]}'
+                #     if score["value"] == 0:
+                #         if is_white:
+                #             scoregraph = 30
+                #         else:
+                #             scoregraph = -30
 
-                    elif score["value"] > 0:
-                        scoregraph = f'{30 - (score["value"])}'
-                    else:
-                        scoregraph = f'{-30 + (score["value"] * -1)}'
+                #     elif score["value"] > 0:
+                #         scoregraph = f'{30 - (score["value"])}'
+                #     else:
+                #         scoregraph = f'{-30 + (score["value"] * -1)}'
 
                 if prev_fen and detailed:
                     if abs(score_diff) >= 450:
@@ -133,7 +206,7 @@ def my_form_post():
                 if topstr.find(f'{piece}{square}') == -1 and topstr != '':
                     notfound = '*'
 
-                line = f'{highlight} {movenostr}  ({scorestr})  {piece}{square}{notfound}            {topstr} {highlight_suf}'
+                line = f'{highlight} {movenostr}  ({scorestr})  {piece}{square}{notfound}            {topstr} {highlight_suf} '
                 output += f'{line}<br>\n'
                 moves.append([highlight, movenostr, scorestr, piece, square, top_moves_str, highlight_suf, scoregraph])
                 print(line)
@@ -146,7 +219,8 @@ def my_form_post():
                 # print(stockfish.get_top_moves(3))
                 # print(stockfish.get_best_move())
     # return output
-    return render_template('index.html', moves=moves,pgn=pgn)
+
+    return render_template('index.html', moves=moves,pgn=pgn, time=current_milli_time()-start_time)
 
 
 
